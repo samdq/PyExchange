44d4adc90f37f8c866fc8edacc325073496150d8
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2019-01-30 18:02:10 +0530

    update file

--- calculator.py
+++ calculator.py
@@ -1,8 +1,2 @@
-# geometric_calculator/calculator.py
 from .shapes import Circle, Rectangle, Triangle
-
-class Calculator:
-    @staticmethod
-    def calculate_area(shape, *args):
-        if shape.lower() == 'circle':
 
