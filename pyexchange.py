e77eaa912263667a4e52f779a54d35df74897a8e
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2019-04-17 18:18:49 +0530

    update file

--- pyexchange.py
+++ pyexchange.py
@@ -4,4 +4,6 @@ import requests
 from exchange_rates import get_exchange_rate
 
 def convert_currency(amount, from_currency, to_currency):
-    
+    rate = get_exchange_rate(from_currency, to_currency)
+    if rate is not None:
+        converted_amount = amount * rate
