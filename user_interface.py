552d5af6e0f40f379b52dbebb599bd9d6bfeea46
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2019-05-06 18:22:59 +0530

    update file

--- user_interface.py
+++ user_interface.py
@@ -6,4 +6,8 @@ if __name__ == "__main__":
     from_currency = input("Enter the source currency code: ")
     to_currency = input("Enter the target currency code: ")
 
+    result = convert_currency(amount, from_currency, to_currency)
+
+    if result is not None:
+        print(f"{amount} {from_currency} is equal to {result:.2f} {to_currency}")
 
