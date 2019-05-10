73d4a8e2536a6e04eecfd9c1493177255f34c77c
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2019-05-10 18:24:40 +0530

    update file

--- shapes.py
+++ shapes.py
@@ -2,20 +2,4 @@
 
 import math
 
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * self.radius ** 2
-
-
-class Rectangle:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def area(self):
-        return self.length * self.width
-
 
