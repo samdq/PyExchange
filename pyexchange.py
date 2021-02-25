fe6331b09e9f3dcf745bc60f07bec7e2ab747619
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2021-02-25 10:52:08 +0530

    update file

--- pyexchange.py
+++ pyexchange.py
@@ -15,10 +15,3 @@ if __name__ == "__main__":
     amount = float(input("Enter the amount to convert: "))
     from_currency = input("Enter the source currency code: ")
     to_currency = input("Enter the target currency code: ")
-
-    result = convert_currency(amount, from_currency, to_currency)
-
-    if result is not None:
-        print(f"{amount} {from_currency} is equal to {result:.2f} {to_currency}")
-    else:
-        print("Unable to fetch exchange rate. Please check your input and try again.")
