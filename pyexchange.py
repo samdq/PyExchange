500c91d16fe263802c1f34e8b9d495e966cadd2c
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2021-02-27 10:52:25 +0530

    update file

--- pyexchange.py
+++ pyexchange.py
@@ -8,10 +8,3 @@ def convert_currency(amount, from_currency, to_currency):
     if rate is not None:
         converted_amount = amount * rate
         return converted_amount
-    else:
-        return None
-
-if __name__ == "__main__":
-    amount = float(input("Enter the amount to convert: "))
-    from_currency = input("Enter the source currency code: ")
-    to_currency = input("Enter the target currency code: ")
