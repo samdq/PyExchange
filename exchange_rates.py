2721d5688ccc1351f15ec9e3b551a89523dc6734
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2021-03-08 10:55:46 +0530

    update file

--- exchange_rates.py
+++ exchange_rates.py
@@ -6,3 +6,7 @@ def get_exchange_rate(from_currency, to_currency):
     # Implement logic to fetch exchange rates from CurrencyLayer or Fixer.io
     # Use the API keys stored in api_key.txt
     # Example using CurrencyLayer API
+    currency_layer_api_key = get_api_key("CURRENCYLAYER")
+    currency_layer_url = f"https://api.currencylayer.com/live?access_key={currency_layer_api_key}"
+    response = requests.get(currency_layer_url)
+    data = response.json()
