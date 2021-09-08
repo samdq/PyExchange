b1f15fd88bc215ee54d7b20eeb9a27359c796b06
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2021-09-08 11:10:53 +0530

    update file

--- user_interface.py
+++ user_interface.py
@@ -2,3 +2,5 @@
 
 from pyexchange import convert_currency
 if __name__ == "__main__":
+    amount = float(input("Enter the amount to convert: "))
+    from_currency = input("Enter the source currency code: ")
