adbad6356d79bd21bd528969ccdb79f6ce12dc21
Linus XCJ <70716538+linusxcj@users.noreply.github.com>
2021-09-10 11:11:31 +0530

    update file

--- user_interface.py
+++ user_interface.py
@@ -5,3 +5,5 @@ if __name__ == "__main__":
     amount = float(input("Enter the amount to convert: "))
     from_currency = input("Enter the source currency code: ")
     to_currency = input("Enter the target currency code: ")
+
+    result = convert_currency(amount, from_currency, to_currency)
